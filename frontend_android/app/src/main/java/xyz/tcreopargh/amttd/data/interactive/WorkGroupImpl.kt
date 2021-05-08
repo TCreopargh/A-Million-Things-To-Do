38e889a75d3f507a1275febc86dd3e0cb7f9855e
TCreopargh <TCreopargh@gmail.com>
2021-05-08 22:55:38 +0800

    fix

--- frontend_android/app/src/main/java/xyz/tcreopargh/amttd/data/interactive/WorkGroupImpl.kt
+++ frontend_android/app/src/main/java/xyz/tcreopargh/amttd/data/interactive/WorkGroupImpl.kt
@@ -6,6 +6,12 @@ data class WorkGroupImpl(
     override var groupId: UUID = UUID.randomUUID(),
     override var name: String = "",
     override var timeCreated: Calendar = Calendar.getInstance(),
-): IWorkGroup {
-    constructor(workGroup: IWorkGroup) : this(workGroup.groupId, workGroup.name, workGroup.timeCreated)
+    override var usersInGroup: MutableList<UserImpl> = mutableListOf()
+) : IWorkGroup {
+    constructor(workGroup: IWorkGroup) : this(
+        workGroup.groupId,
+        workGroup.name,
+        workGroup.timeCreated,
+        workGroup.usersInGroup.map { UserImpl(it) }.toMutableList()
+    )
 }
