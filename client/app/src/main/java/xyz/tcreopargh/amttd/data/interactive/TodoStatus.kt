6b7cdbf0cb194baa1c4d2f64e4b095c0902e0b31
TCreopargh <TCreopargh@gmail.com>
2021-05-13 12:30:31 +0800

    Fix color

--- client/app/src/main/java/xyz/tcreopargh/amttd/data/interactive/TodoStatus.kt
+++ client/app/src/main/java/xyz/tcreopargh/amttd/data/interactive/TodoStatus.kt
@@ -10,7 +10,7 @@ import java.io.Serializable
 /**
  * @author TCreopargh
  */
-enum class TodoStatus(@ColorInt colorInt: Int, val sortOrder: Int) : Serializable {
+enum class TodoStatus(@ColorInt val colorInt: Int, val sortOrder: Int) : Serializable {
     NOT_STARTED(0xf44336, 2),
     IN_PLAN(0x3f51b5, 1),
     IN_PROGRESS(0x2196f3, 0),
@@ -18,7 +18,7 @@ enum class TodoStatus(@ColorInt colorInt: Int, val sortOrder: Int) : Serializabl
     ON_HOLD(0x607d8b, -1),
     CANCELLED(0x9e9e9e, -3);
 
-    val color = 0xff shl 24 or colorInt
+    val color get() = 0xff shl 24 or colorInt
 
     fun isFinished() = this == COMPLETED || this == CANCELLED
     fun isActive() = !(this == ON_HOLD || this == CANCELLED || this == COMPLETED)
