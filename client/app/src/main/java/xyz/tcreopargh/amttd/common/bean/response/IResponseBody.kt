ccf13b8efaab7a1d1570a9d2b7508ad0e149db8a
TCreopargh <TCreopargh@gmail.com>
2021-05-16 20:57:56 +0800

    Edit workgroup (Read only at this time)

--- client/app/src/main/java/xyz/tcreopargh/amttd/common/bean/response/IResponseBody.kt
+++ client/app/src/main/java/xyz/tcreopargh/amttd/common/bean/response/IResponseBody.kt
@@ -3,6 +3,6 @@ package xyz.tcreopargh.amttd.common.bean.response
 import java.io.Serializable
 
 interface IResponseBody : Serializable {
-    var success: Boolean?
-    var error: Int?
+    val success: Boolean?
+    val error: Int?
 }
