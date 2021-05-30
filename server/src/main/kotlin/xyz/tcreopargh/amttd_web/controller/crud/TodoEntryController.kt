b61f1985b6348cfc88569c1307c3141ff8bef4de
TCreopargh <TCreopargh@gmail.com>
2021-05-30 20:30:05 +0800

    Update comments

--- server/src/main/kotlin/xyz/tcreopargh/amttd_web/controller/crud/TodoEntryController.kt
+++ server/src/main/kotlin/xyz/tcreopargh/amttd_web/controller/crud/TodoEntryController.kt
@@ -24,6 +24,24 @@ import javax.servlet.http.HttpServletRequest
 @RestController
 @LoginRequired
 class TodoEntryController : ControllerBase() {
+
+    /**
+     * Respond to add, edit, view and delete operations on a single to-do entry entity.
+     *
+     * [body] The request body. CrudType indicates which action to perform,
+     * but a controller might not implement all 4 types of operations.
+     *
+     * [TodoEntryCrudRequest.entity] represents the entity associated with this action.
+     * Only necessary properties of it may be used in the controller to decide how to create/update the entity.
+     * If the operation is `VIEW` or `DELETE`, only the id should be used.
+     *
+     * Otherwise, the id should be ignored because server should decide what ID an entity should have, not the client
+     * In this case, the id should be generated again on the server.
+     *
+     * @return An response of the operation. [TodoEntryCrudResponse.success] indicates if the operation is successful.
+     * Serialize errors to error codes and pass them via [TodoEntryCrudResponse.error] if the operation failed.
+     * [TodoEntryCrudResponse.entity] indicates the new entity. If not needed, this can be set to null.
+     */
     @PostMapping(
         "/todo-entry",
         consumes = [MediaType.APPLICATION_JSON_VALUE],
@@ -39,17 +57,22 @@ class TodoEntryController : ControllerBase() {
                 verifyTodoEntry(request, body.entity?.entryId)
             }
             when (body.operation) {
-                CrudType.READ -> {
+                CrudType.READ   -> {
                     var entry: EntityTodoEntry? = null
                     body.entity?.entryId?.let {
                         entry = todoEntryService.findByIdOrNull(it)
-                    }
+                    } ?: throw AmttdException(AmttdException.ErrorCode.JSON_NON_NULLABLE_VALUE_IS_NULL)
                     val ret = entry?.let { TodoEntryImpl(it) }
                         ?: throw AmttdException(AmttdException.ErrorCode.REQUESTED_ENTITY_NOT_FOUND)
                     TodoEntryCrudResponse(operation = CrudType.READ, success = true, entity = ret)
                 }
-                // TODO: Implement other actions
-                else          -> TodoEntryCrudResponse(
+                CrudType.CREATE -> {
+                    TODO("Add new to-do entry")
+                }
+                CrudType.DELETE -> {
+                    TODO("Remove to-do entry")
+                }
+                else            -> TodoEntryCrudResponse(
                     success = false,
                     error = AmttdException.ErrorCode.ACTION_NOT_SUPPORTED.value
                 )
@@ -57,7 +80,7 @@ class TodoEntryController : ControllerBase() {
         } catch (e: Exception) {
             logger.error("Error in todo entry crud: ", e)
             TodoEntryCrudResponse(
-                operation = CrudType.READ,
+                operation = body.operation,
                 success = false,
                 error = AmttdException.ErrorCode.getFromException(e).value
             )
